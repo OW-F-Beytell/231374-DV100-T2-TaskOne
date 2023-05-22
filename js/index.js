85b5821a21031514f853999e691a79b7af9f815a
OW-F-Beytell <125928514+OW-F-Beytell@users.noreply.github.com>
2023-05-23 00:10:36 +0200

    Exercise One and Two

---
+++ js/index.js
@@ -0,0 +1,20 @@
+console.log("Hello world!");
+
+/* Exercise One */
+let firstName = "Freddy";
+let lastName = "Beytell";
+let age = 21;
+const subject = "Development";
+
+let greeter = "Hello, my name is " + firstName + " " + lastName + ", I am " + age + " years old and I am studying " + subject;
+console.log(greeter); 
+
+/* Exercise Two */
+let numOne = 1;
+let numTwo = 2;
+let numThree = 3;
+let numFour = 4;
+let answer = 0;
+
+answer = numOne + numTwo / numThree * numFour;
+console.log(answer);
