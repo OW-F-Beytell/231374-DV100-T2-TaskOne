b07ece577426baf7c59328ebf48361a69334c48f
OW-F-Beytell <125928514+OW-F-Beytell@users.noreply.github.com>
2023-05-23 02:53:38 +0200

    Exercise Four

--- js/index.js
+++ js/index.js
@@ -27,3 +27,45 @@ let susanRate = 0.26;
 
 let Value = (timmyAmount * timmyRate) + (susanAmount * susanRate);
 console.log(Value);
+
+/* Exercise Four */
+let johnValue = 75;
+let timValue = 60;
+let alfieValue = 64;
+let gwenValue = 93;
+let averageValue = (johnValue + timValue + alfieValue + gwenValue) / 4;
+let testMax = 100;
+
+let johnPerc = johnValue / testMax * 100;
+let timPerc = timValue / testMax * 100;
+let alfiePerc = alfieValue / testMax * 100;
+let gwenPerc = gwenValue / testMax * 100;
+let averagePerc = averageValue / testMax * 100;
+
+
+console.log("Results:");
+console.log("John: " + johnPerc + "%");
+console.log("Tim: " + timPerc + "%");
+console.log("Alfie: " + alfiePerc + "%");
+console.log("Gwen: " + gwenPerc + "%");
+console.log("Average: " + johnPerc + "%");
+console.log("");
+
+johnValue += 7;
+timValue += 7;
+alfieValue += 7;
+gwenValue += 7;
+averageValue = (johnValue + timValue + alfieValue + gwenValue) / 4;
+
+johnPerc = johnValue / testMax * 100;
+timPerc = timValue / testMax * 100;
+alfiePerc = alfieValue / testMax * 100;
+gwenPerc = gwenValue / testMax * 100;
+averagePerc = averageValue / testMax * 100;
+
+console.log("After adjusting grades:");
+console.log("John: " + johnPerc + "%");
+console.log("Tim: " + timPerc + "%");
+console.log("Alfie: " + alfiePerc + "%");
+console.log("Gwen: " + gwenPerc + "%");
+console.log("Average: " + johnPerc + "%");
